db12da3f51322963d4f46f524a1aa9f93fa9010a
Hori Godai <ess_president@me.com>
2018-08-29 09:12:58 +0900

    added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare module 'scroll-hint' {
+
+  interface i18n {
+    scrollable: string
+  }
+  
+  interface ScrollHintOption {
+    suggestClass?: string,
+    scrollableClass?: string,
+    scrollableRightClass?: string,
+    scrollableLeftClass?: string,
+    scrollHintClass?: string,
+    scrollHintIconClass?: string,
+    scrollHintIconAppendClass?: string,
+    scrollHintIconWrapClass?: string,
+    scrollHintText?: string,
+    scrollHintBorderWidth?: number,
+    remainingTime?: string,
+    enableOverflowScrolling?: boolean,
+    applyToParents?: boolean,
+    suggestiveShadow?: boolean,
+    i18n?: i18n
+  }
+
+  export default class ScrollHint {
+    constructor(selector: string | HTMLElement, option?:ScrollHintOption);
+    updateItems(): void;
+  }
+}
